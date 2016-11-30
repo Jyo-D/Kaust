2ca88631516e7886cba36b36646015584d34674a
Kunal <kunal@incture.com>
2016-11-30 14:49:05 +0530

    util package

---
+++ Kaust/src/utility/Constant.java
@@ -0,0 +1,21 @@
+package utility;
+
+public class Constant {
+	    public static final String URL = "https://sthcibpdqq1.kaust.edu.sa:50001/dashboard/index.html";
+	    
+		public static final String Path_TestData = "D://Incture//Selenium//FD_Test//Kaust//src//testData//";
+		public static final String File_TestData = "TestData.xlsx";
+		
+		//Test Data Sheet Columns
+		public static final int Col_TestCaseName = 0;	
+		public static final int Col_UserName =3 ;
+		public static final int Col_Password = 4;
+		public static final int Col_Browser = 2;
+		public static final int Col_Result = 1;
+		public static final int Col_month = 6;
+		public static final int Col_date = 7;
+		public static final int Col_year = 8;
+		public static final int Col_Lab_Sel = 9;
+		public static final String Path_ScreenShot = "D://Incture//Selenium//FD_Test//Kaust//src//Screenshots//New folde//";
+		
+	}
